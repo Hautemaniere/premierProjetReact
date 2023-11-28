eff8b3767ebc2d88647d7cbd3edf46d0ce7f025c
Hautemaniere <113522498+Hautemaniere@users.noreply.github.com>
2023-11-28 11:45:45 +0100

    add newcomp

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CompByeXo from './composant/newcomp'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <App/>
+  <CompByeXo couleur={'red'}/>
   </React.StrictMode>
 );
 
