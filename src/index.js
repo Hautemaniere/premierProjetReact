f92013cb768724c32aaba12ebe129b1e2e631d20
Hautemaniere <113522498+Hautemaniere@users.noreply.github.com>
2023-11-30 14:26:24 +0100

    show only new comp

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import CompByeXo from './composant/newcomp'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <App/>
+  { /* <App/> */ }
   <CompByeXo couleur={'red'}/>
   </React.StrictMode>
 );
