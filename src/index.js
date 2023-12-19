4b95e671b785f8febf48af90e1ee845560f54f52
Hautemaniere <113522498+Hautemaniere@users.noreply.github.com>
2023-12-19 09:25:02 +0100

    exo 32

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import CompByeXo from './composant/newcomp'
 import ExempleCondition from './exo29/composant29';
 import exo30 from './exo30/composant30';
 import Exo31 from './exo31/composant31';
+import Exo32 from './exo32/composant32';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -17,7 +18,8 @@ root.render(
   {/* <CompByeXo couleur={'red'}/> */}
   {/* <ExempleCondition/> */}
   {/*exo30()*/}
-  <Exo31/>
+  {/*<Exo31/>*/}
+  <Exo32/>
 
   </React.StrictMode>
 );
